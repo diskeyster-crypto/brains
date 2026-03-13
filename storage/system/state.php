8eaf3b2bb6f8d0355292cb1993c331f0962c3444
diskeyster-crypto <diskeyster@gmail.com>
2026-03-13 16:45:01 +0300

    Загрузка файлов с сервера - storage/system/state.php

---
+++ storage/system/state.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * System state storage (auto-generated)
+ * DO NOT EDIT MANUALLY
+ */
+return array (
+  'installed' => true,
+  'installed_at' => '2026-01-26T13:38:01+03:00',
+  'core_version' => '1.0.0',
+  'php_version' => '8.2.29',
+  'admin_user_created' => true,
+  'installer_version' => '1.0.0',
+  'admin_username' => 'admin',
+);

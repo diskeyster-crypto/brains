2015d40bca8c985cb7695f4e2eae08cdd55e8921
diskeyster-crypto <diskeyster@gmail.com>
2026-03-13 16:45:00 +0300

    Загрузка файлов с сервера - storage/system/migrations.php

---
+++ storage/system/migrations.php
@@ -0,0 +1,18 @@
+<?php
+
+// Applied migrations - DO NOT EDIT MANUALLY
+
+return array (
+  '1.0.0' => 
+  array (
+    'applied_at' => '2026-01-25T16:10:31+00:00',
+  ),
+  '1.1.0' => 
+  array (
+    'applied_at' => '2026-01-25T16:10:31+00:00',
+  ),
+  '1.2.0' => 
+  array (
+    'applied_at' => '2026-01-25T16:10:31+00:00',
+  ),
+);

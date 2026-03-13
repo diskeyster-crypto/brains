fc70aa07b3a9d116c19535ff11a340269542abc2
diskeyster-crypto <diskeyster@gmail.com>
2026-03-13 16:38:45 +0300

    Загрузка файлов с сервера - modules/system/smart_brain/config/profiles.php

---
+++ modules/system/smart_brain/config/profiles.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'default_profile' => '111',
+    'profiles' => [
+        '111' => [
+            'budget' => 15.0,
+            'max_leverage' => 5,
+            'stop_loss_range' => 0.20,
+            'slippage_bps' => 20,
+            'trailing_activate_roi' => 0.06,
+            'take_profit_roi' => 5.55,
+        ],
+    ],
+];

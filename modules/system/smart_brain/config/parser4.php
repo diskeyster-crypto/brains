1546f74c7814ddae93cc0ef14e46611405913f05
diskeyster-crypto <diskeyster@gmail.com>
2026-03-13 16:38:43 +0300

    Загрузка файлов с сервера - modules/system/smart_brain/config/parser4.php

---
+++ modules/system/smart_brain/config/parser4.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'enabled' => true,
+    'profiles_key' => 'parser.parser3_manager_behavior.storage',
+    'history_key' => 'parser.parser2_history_accumulator.storage',
+    'corridor_window_minutes' => 120,
+    'min_history_points' => 40,
+    'strength_threshold' => 0.50,
+    'max_candidates' => 200,
+];

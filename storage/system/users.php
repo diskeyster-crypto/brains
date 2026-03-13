92e2187142aa8b365cce28b9dde675beb05d0279
diskeyster-crypto <diskeyster@gmail.com>
2026-03-13 16:45:03 +0300

    Загрузка файлов с сервера - storage/system/users.php

---
+++ storage/system/users.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Users storage (auto-generated)
+ * DO NOT EDIT MANUALLY
+ */
+return array (
+  'admin' => 
+  array (
+    'password' => '$2y$10$0e7EHZS4jsZj6vDm\/WJLY.UYQJbXupB5d0VuSjXHpeIXXdaZd1m9O',
+    'role' => 'admin',
+    'created_at' => '2026-01-26 13:37:56',
+  ),
+);

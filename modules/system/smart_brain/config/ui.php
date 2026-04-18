02e0c118949ae8dce30d92130950bb1e0dea780a
diskeyster-crypto <diskeyster@gmail.com>
2026-04-18 14:09:22 +0300

    Загрузка файлов с сервера - modules/system/smart_brain/config/ui.php

--- modules/system/smart_brain/config/ui.php
+++ modules/system/smart_brain/config/ui.php
@@ -2,13 +2,6 @@
 declare(strict_types=1);
 
 return [
-    'mode' => 'manual',
-    'settings' => [
-        'title' => 'Smart Brain',
-        'refresh_seconds' => 7,
-    ],
-    'auto_rules' => [
-        'allow_brain_override' => false,
-        'override_fields' => [],
-    ],
+    'title' => 'Smart Brain',
+    'refresh_seconds' => 5,
 ];
